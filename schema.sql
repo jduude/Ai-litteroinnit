75eaf96343bdb6865a395604d89ca282ac97061c
Jukka Ylitalo <juylitalo@gmail.com>
2025-11-04 19:44:37 +0200

    add transcriptions table to db

--- schema.sql
+++ schema.sql
@@ -3,4 +3,15 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
- 
+
+
+
+CREATE TABLE transcriptions (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    source_path TEXT,
+    source TEXT,
+    genre TEXT,
+    raw_content TEXT,
+    user_id INTEGER REFERENCES users
+);
