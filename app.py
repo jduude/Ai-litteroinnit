361eea358f491f7e0e51e1cff3b73ba53dbd89c4
Jukka Ylitalo <juylitalo@gmail.com>
2025-11-04 19:30:10 +0200

    add login template and get handler

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ app = Flask(__name__)
 def index():
     return render_template("index.html" )
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
