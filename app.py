4b7809db1ddebd087b92ebb6bbb7556d9b245dfc
Jukka Ylitalo <juylitalo@gmail.com>
2025-11-04 19:28:31 +0200

    add user table and register user account endpoints

--- app.py
+++ app.py
@@ -1,6 +1,51 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request
+from werkzeug.security import generate_password_hash
+import db
+import  users
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html" )
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+
+        if password1 != password2:
+            return "VIRHE: salasanat eivät ole samat"
+
+        try:
+            users.create_user(username, password1)
+            return "Tunnus luotu"
+        except sqlite3.IntegrityError:
+            return "VIRHE: tunnus on jo varattu"
+
+
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+
+    return "Tunnus luotu"
---
+++ users.py
@@ -0,0 +1,9 @@
+from werkzeug.security import generate_password_hash
+import db
+
+def create_user(username, password):
+    password_hash = generate_password_hash(password)
+    sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+    db.execute(sql, [username, password_hash])
+
+ 
