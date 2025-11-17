f6d1b7f023afc1841fe984fd7e15ecd13f78e063
Jukka Ylitalo <juylitalo@gmail.com>
2025-11-17 19:47:15 +0200

    add fallback filename for audio file using full path stored in database

--- app.py
+++ app.py
@@ -78,6 +78,13 @@ def show_transcription(transcription_id, page=1):
     local_audio_file_copy = os.path.join('static/audio', source_path_file_name)
     local_audio_file_copy_exists= os.path.exists(local_audio_file_copy)
     audio_file_path = f"audio/{source_path_file_name}"
+    if not local_audio_file_copy_exists:
+        # filenames from the whole source_path string
+        source_path_fallback = source_path.replace("\\",'--')
+        local_audio_file_copy = os.path.join('static/audio', source_path_fallback)
+        local_audio_file_copy_exists= os.path.exists(local_audio_file_copy)
+        audio_file_path = f"audio/{source_path_fallback}"
+
 
     return render_template("transcription.html", transcription=transcription, text_fragments=text_fragments_with_secs, 
                            convert_seconds_to_hms=help_functions.convert_seconds_to_hms,
