b106a019b9a14fd9efda13853d913c46ea08239f
Jukka Ylitalo <juylitalo@gmail.com>
2026-01-08 21:46:54 +0200

    set secret key in env var

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-SECRET_KEY = "jlkj23aldoHKJH73pjkHasdlammmmmm"
+import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY environment variable must be set")
